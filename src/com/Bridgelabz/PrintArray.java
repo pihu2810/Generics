9f6468dbb17bd968fe9ddc45d73cb958875cbea5
pihu2810 <vishaka.kadam01@gmail.com>
2022-04-04 12:37:02 +0530

    add printArrayUC2

--- src/com/Bridgelabz/PrintArray.java
+++ src/com/Bridgelabz/PrintArray.java
@@ -3,31 +3,20 @@ package com.Bridgelabz;
 public class PrintArray 
 {
 	 public static void main(String[] args) {
-		  int[] a= {1,2,3,4,5};
-		  double[] b= {1.1, 2.2, 3.3,4.4, 5.5};
-		  char[] c={'A','B','C','d'};
-		  toprint(a);
+		 Integer[] a= {1,2,3,4,5};
+		  Double[] b= {1.1, 2.2, 3.3,4.4, 5.5};
+		  Character[] c={'A','B','C','d'};
+		  toPrint(a);
 		  toPrint(b);
 		  toPrint(c);
 	  }
 	  
-private static void toPrint(char[] c) {
-	  for(char i:c) {
-			System.out.println(i);
-		}
+private static <E> void toPrint(E[] a) {
+	for(E i:a) {
+		System.out.println(i);
+	}
 		
 	}
 
-private static void toPrint(double[] b) {
-	  for(double i:b) {
-			System.out.println(i);
-		}
-		
-	}
 
-private static void toprint(int[] a) {
-		for(int i:a) {
-			System.out.println(i);
-		}
-		}
 }
