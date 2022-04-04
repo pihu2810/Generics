cb8547dad90f7bfb3411372e0db318605e4f3d42
pihu2810 <vishaka.kadam01@gmail.com>
2022-04-04 14:28:30 +0530

    add Refactoring the Using Generics Method UC3

--- src/com/Bridgelabz/MaximumComputation.java
+++ src/com/Bridgelabz/MaximumComputation.java
@@ -12,8 +12,9 @@ public class MaximumComputation
 	  findMaximum(x1,y1,z1);
 	  findMaximum(x2,y2,z2);
   }
-  private static void findMaximum(Integer x, Integer y, Integer z) {
-		Integer maxNumber;
+  public static<E extends Comparable<E>>void findMaximum(E x,E y,E z)
+  {
+	  E maxNumber;
 		if(x.compareTo(y)>0 && x.compareTo(z)>0) {
 			maxNumber=x;
 		}
@@ -26,33 +27,6 @@ public class MaximumComputation
 		}
 		System.out.println("The MaximumNumber is : " +maxNumber);
 	}
-  private static void findMaximum(Float x1, Float y1, Float z1) {
-		Float maxNumber;
-		if(x1.compareTo(y1)>0 && x1.compareTo(z1)>0) {
-			maxNumber=x1;
-		}
-		else if(y1.compareTo(x1)>0 && y1.compareTo(z1)>0) {
-			maxNumber=y1;
-		}
-		else
-		{
-		maxNumber=z1;
-		}
-		System.out.println("The MaximumNumber is : " +maxNumber);
-	}
-
-private static void findMaximum(String x2, String y2, String z2) {
-	String maxNumber;
-	if(x2.compareTo(y2)>0 && x2.compareTo(z2)>0) {
-		maxNumber=x2;
-	}
-	else if(y2.compareTo(x2)>0 && y2.compareTo(z2)>0) {
-		maxNumber=y2;
-	}
-	else
-	{
-	maxNumber=z2;
-	}
-	System.out.println("The MaximumString is : " +maxNumber);
-}
+  
+  
 }
