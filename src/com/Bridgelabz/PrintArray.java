1819dc3007a7ba554dc206be35e11595194667f1
pihu2810 <vishaka.kadam01@gmail.com>
2022-04-04 13:36:21 +0530

    add printArrayUsingGenericClassUC3

--- src/com/Bridgelabz/PrintArray.java
+++ src/com/Bridgelabz/PrintArray.java
@@ -1,16 +1,32 @@
 package com.Bridgelabz;
 
-public class PrintArray 
+public class PrintArray <E>
 {
-	 public static void main(String[] args) {
+  E[]MyArray;
+	
+	 public PrintArray(E[] myArray) {
+	super();
+	this.MyArray = myArray;
+}
+
+	public static void main(String[] args) {
 		 Integer[] a= {1,2,3,4,5};
 		  Double[] b= {1.1, 2.2, 3.3,4.4, 5.5};
 		  Character[] c={'A','B','C','d'};
 		  toPrint(a);
 		  toPrint(b);
 		  toPrint(c);
+		  new PrintArray<Integer>(a).toPrint();
+		  new PrintArray<Double>(b).toPrint();
+		  new PrintArray<Character>(c).toPrint();
+		  
 	  }
 	  
+private void toPrint() {
+		toPrint(MyArray);
+		
+	}
+
 private static <E> void toPrint(E[] a) {
 	for(E i:a) {
 		System.out.println(i);
